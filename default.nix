bea4c7098830b3c496ecd622efcf2aaf01c35ad6
Jon Zuka <jon.zuka.42@gmail.com>
2024-12-09 16:00:21 +0100

    add matplotlib

--- default.nix
+++ default.nix
@@ -2,6 +2,11 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     jupyter-all
+    (
+      with jupyter-all.pkgs; [
+        matplotlib
+      ]
+    )
   ];
   JUPYTER_CONFIG_DIR = "./config";
 }
